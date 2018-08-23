efde98ecdb73b8f750ee19bac8a6e3d8c0ec47db
supriyagogineni <42571288+supriyagogineni@users.noreply.github.com>
2018-08-22 23:11:08 -1200

    Update even.py

--- even.py
+++ even.py
@@ -1,7 +1,7 @@
-suppi = int(input(""))
-if suppi % 2 == 0:
+devil = int(input(""))
+if devil % 2 == 0:
 	print("even")
-elif suppi % 2 != 0:
+elif devil % 2 != 0:
 	print("odd")
 else:
 	print("invalid")
