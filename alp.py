daf136f300efc76807bf00df4641216533f85325
supriyagogineni <42571288+supriyagogineni@users.noreply.github.com>
2018-08-30 16:19:39 -1200

    Update and rename alphabet.py to alp.py

---
+++ alp.py
@@ -0,0 +1,5 @@
+alphabets = raw_input()
+if alphabets in ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'):
+    print("Alphabet")
+else:
+    print("No")
