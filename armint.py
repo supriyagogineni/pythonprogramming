298484f6fa586cca89116c302389d093d6c0fa2f
supriyagogineni <42571288+supriyagogineni@users.noreply.github.com>
2018-08-31 18:56:39 -1200

    Create armint.py

---
+++ armint.py
@@ -0,0 +1,11 @@
+l,h = map(int,raw_input().split())
+for n in range(l,h):
+    order =len(str(n))
+    sum = 0
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    if n == sum:
+        print (n),
